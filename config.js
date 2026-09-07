7cbb3af5b463dd398b434b9fa90e0a82381243be
portlogistics.vn <portlogistics.lab@gmail.com>
2026-09-07 14:06:59 +0700

    Update frontend to v255 backend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'https://smart-warehouse-backend.onrender.com',
-  APP_VERSION: '2.5.5-web.3'
+  API_BASE_URL: 'https://smart-warehouse-v255-backend.onrender.com',
+  APP_VERSION: '2.5.5-web.4'
 };
